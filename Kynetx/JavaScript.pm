7eca849ec46828b06a31eaf6915fb0cec2e7db3f
web <web@e71f4da1-d517-483a-a2ea-a46d7071bf44>
2008-06-20 20:02:57 +0000

    fixed errordoc directive for krl
    git-svn-id: svn+ssh://svn.kynetx.com/perl@114 e71f4da1-d517-483a-a2ea-a46d7071bf44

--- Kynetx/JavaScript.pm
+++ Kynetx/JavaScript.pm
@@ -117,7 +117,7 @@ sub gen_js_decl {
 	    $val = Kynetx::Predicates::Weather::get_weather($req_info,$function);
 	} elsif ($source eq 'geoip' || $source eq 'location') {
 	    $val = Kynetx::Predicates::Location::get_geoip($req_info,$function);
-	} elsif ($source eq 'stocks') {
+	} elsif ($source eq 'stocks' || $ source eq 'markets') {
 	    my @arg = gen_js_rands($decl->{'args'});
 	    $arg[0] =~ s/'([^']*)'/$1/;  # cheating here to remove JS quotes
 	    $val = Kynetx::Predicates::Markets::get_stocks($req_info,$arg[0],$function);
