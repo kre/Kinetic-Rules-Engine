3e56cd912b7463e3eeead63d07611fb0a93bfb1c
sam <sam@e71f4da1-d517-483a-a2ea-a46d7071bf44>
2010-01-14 22:24:52 +0000

    Updating Library to jQuery 1.4 (passed AutoJAM)
    git-svn-id: svn+ssh://svn.kynetx.com/perl@395 e71f4da1-d517-483a-a2ea-a46d7071bf44

--- bin/install-init-files.pl
+++ bin/install-init-files.pl
@@ -34,7 +34,7 @@ my $hstamp = $dt->hms('');
 my $kobj_file = "kobj-static-".$dstamp.$hstamp.".js";
 
 my @js_files = qw(
-jquery-1.3.2.js
+jquery-1.4.js
 jquery.json-1.2.js
 jquery-ui-1.7.2.custom.js
 kgrowl-1.0.js
