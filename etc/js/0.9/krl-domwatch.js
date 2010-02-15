89bc35285245c85531e4e5536b5ee0f6a3a93267
sam <sam@e71f4da1-d517-483a-a2ea-a46d7071bf44>
2010-02-15 18:40:53 +0000

    jam update to domwatch
    git-svn-id: svn+ssh://svn.kynetx.com/perl@430 e71f4da1-d517-483a-a2ea-a46d7071bf44

--- etc/js/0.9/krl-domwatch.js
+++ etc/js/0.9/krl-domwatch.js
@@ -2,47 +2,64 @@ KOBJ.watchDOM = function(selector,callBackFunc,time){
   if(!KOBJ.watcherRunning){
     KOBJ.log("Starting the DOM Watcher");
     var KOBJ_setInterval = 0;
-    if(!typeof(setInterval_native) == "undefined"){
+    if(typeof(setInterval_native) != "undefined"){
       KOBJ_setInterval = setInterval_native;
     } else {
       KOBJ_setInterval = setInterval;
     }
     if(KOBJ.watcherRunning){clearInterval(KOBJ.watcherRunning);}
-    KOBJ.watcherData = [];
-    KOBJ.watcherData.push({"selector": selector,"callBacks": [callBackFunc]});
-    KOBJ.log("DOM Watcher Callback for new selector " +selector+ " added");
-    $K(selector+" :first").addClass("KOBJ_AjaxWatcher");
-    KOBJ.watcher = function(){
-      $K(KOBJ.watcherData).each(function(){
-				  var data = this;
-				  var selectorExists = $K(selector).length;
-				  if(!selectorExists){return;}
-				  var hasNotChanged = $K(data.selector+" :first").is(".KOBJ_AjaxWatcher");
-				  if(!hasNotChanged){
-				    $K(data.callBacks).each(function(){
-							      callBack = this;
-							      KOBJ.log("Running call back on selector " + selector);
-							      callBack();
-							    });
-				    $K(data.selector+" :first").addClass("KOBJ_AjaxWatcher");
-				  }
-      });
-    };
-    KOBJ.watcherRunning = KOBJ_setInterval(KOBJ.watcher,time||1000);
-  } else {
+	    KOBJ.watcherData = KOBJ.watcherData || [];
+	    KOBJ.log("DOM Watcher Callback for new selector " +selector+ " added");
+	    $K(selector+" :first").addClass("KOBJ_AjaxWatcher");
+	    var there = false;
+	    if($K(selector+" :first").is(".KOBJ_AjaxWatcher")){
+		    there = true;
+	    }
+	    KOBJ.watcherData.push({"selector": selector,"callBacks": [callBackFunc], "there": there});
+	    KOBJ.watcher = function(){
+	      $K(KOBJ.watcherData).each(function(){
+					  var data = this;
+					  var selectorExists = $K(selector).length;
+					  if(!selectorExists|| !data.there){return;}
+					  var hasNotChanged = $K(data.selector+" :first").is(".KOBJ_AjaxWatcher");
+					  if(!hasNotChanged){
+					    $K(data.callBacks).each(function(){
+								      callBack = this;
+								      KOBJ.log("Running call back on selector " + selector);
+								      callBack();
+								    });
+					    $K(data.selector+" :first").addClass("KOBJ_AjaxWatcher");
+					  }
+	      });
+	    };
+	KOBJ.watcherRunning = KOBJ_setInterval(KOBJ.watcher,time||500);
+   } else {
 		$K(KOBJ.watcherData).each(function(){
 			dataObj = this;
 			if(dataObj.selector == selector){
-			  dataObj.callBacks.push(callBackFunc);
-			  $K(selector+" :first").addClass("KOBJ_AjaxWatcher");
-			  KOBJ.log("DOM Watcher Callback for previous selector " +selector+ " added");
-			  return false;//breaks out of the loop.
+				dataObj.callBacks.push(callBackFunc);
+				$K(selector+" :first").addClass("KOBJ_AjaxWatcher");
+				
+				if($K(selector+" :first").is(".KOBJ_AjaxWatcher")){
+					dataObj.there = true;
+				} else {
+					dataObj.there = false;
+				}
+			
+				KOBJ.log("DOM Watcher Callback for previous selector " +selector+ " added");
+				return false;//breaks out of the loop.
+			
 			} else {
-			  KOBJ.watcherData.push({"selector": selector,"callBacks": [callBackFunc]});
-			  $K(selector+" :first").addClass("KOBJ_AjaxWatcher");
-			  KOBJ.log("DOM Watcher Call for new selector "+selector+" added");
+				var there = false;
+				
+				if($K(selector+" :first").is(".KOBJ_AjaxWatcher")){
+					there = true;
+				}
+
+				KOBJ.watcherData.push({"selector": selector,"callBacks": [callBackFunc], "there": there});
+				$K(selector+" :first").addClass("KOBJ_AjaxWatcher");
+				KOBJ.log("DOM Watcher Call for new selector "+selector+" added");
 			}
 		});//end each
   }//end if/else
 };
-
