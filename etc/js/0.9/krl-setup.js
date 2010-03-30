2cd6575a378a7099cf2c5bf16ab9e0d11d0939a6
sam <sam@e71f4da1-d517-483a-a2ea-a46d7071bf44>
2010-03-30 21:15:49 +0000

    fixes CSS dependancy
    git-svn-id: svn+ssh://svn.kynetx.com/perl@457 e71f4da1-d517-483a-a2ea-a46d7071bf44

--- etc/js/0.9/krl-setup.js
+++ etc/js/0.9/krl-setup.js
@@ -27,3 +27,26 @@ KOBJ.logger = function(type,txn_id,element,url,sense,rule,rid) {
   if(rid) url+="&rid="+rid;
   KOBJ.require(url);
 };
+
+KOBJ.css=function(css){
+   var head=KOBJ.document.getElementsByTagName('head')[0],
+       style=KOBJ.document.createElement('style'),
+       rules=KOBJ.document.createTextNode(css);
+   style.type='text/css';
+   style.id='KOBJ_stylesheet';
+   KOBJstyle=KOBJ.document.getElementById('KOBJ_stylesheet');
+   if(KOBJstyle==null){
+       if(style.styleSheet){
+           style.styleSheet.cssText=rules.nodeValue;
+       }else{
+           style.appendChild(rules);
+       }
+   head.appendChild(style);
+   }else{
+       if(KOBJstyle.styleSheet){
+           KOBJstyle.styleSheet.cssText+=rules.nodeValue;
+       }else{
+           KOBJstyle.appendChild(rules);
+       }
+   }
+};
