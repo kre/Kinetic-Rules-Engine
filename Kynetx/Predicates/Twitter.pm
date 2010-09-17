9004a8f5885344c341bc59fb2d8adc19e158a63b
Phil Windley <pjw@kynetx.com>
2010-09-17 12:13:09 -0600

    add update to Twitter module

--- Kynetx/Predicates/Twitter.pm
+++ Kynetx/Predicates/Twitter.pm
@@ -447,6 +447,11 @@ my $func_name = {
 				     "parameters" => [qw/date exclude/],
 				     "required" => [qw/none/],
 				    },
+		 'update' => {
+			     "name" => "update",
+			     "parameters" => [qw/status lat long place_id display_coordinates in_reply_to_status_id/],
+			     "required" => [qw/status/]
+			    }
 };
 
 
