e361436761df29d855c3bd6208dc19717a0b0418
meh <meh@kynetx.com>
2011-01-24 11:18:20 -0700

    Catch nulls from being expressed in the javascript

--- Kynetx/JavaScript.pm
+++ Kynetx/JavaScript.pm
@@ -126,7 +126,11 @@ sub gen_js_expr {
 		);		 
 	};
 
-    if (defined $jsexp && $jsexp ne '') {
+    if (defined $jsexp) {
+    	if ($jsexp eq '') {
+    		$logger->debug("Empty expression: ",$expr->{'type'});
+    		return mk_js_str('UNTRANSLATABLE KRL EXPRESSION');
+    	}
     	return $jsexp;
     } else {
     	return mk_js_str('UNTRANSLATABLE KRL EXPRESSION');
@@ -277,9 +281,9 @@ sub gen_js_datasource {
     my $val = '';
 
     if($source eq 'page') {
-	if ($function eq 'id') {
-	    $val = "\$K(".$args->[0].").html()";
-	}
+		if ($function eq 'id') {
+	    	$val = "\$K(".$args->[0].").html()";
+		}
     }
 
     return $val;
