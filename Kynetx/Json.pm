de6971f2647a4bf41296232e35f4b28036d3e066
PJW <pjw@kynetx.com>
2011-09-23 10:19:07 -0600

    changed license terms to GPLv2

--- Kynetx/Json.pm
+++ Kynetx/Json.pm
@@ -2,33 +2,23 @@ package Kynetx::Json;
 
 # file: Kynetx/Json.pm
 #
-# Copyright 2007-2009, Kynetx Inc.  All rights reserved.
+# This file is part of the Kinetic Rules Engine (KRE)
+# Copyright (C) 2007-2011 Kynetx, Inc. 
 #
-# This Software is an unpublished, proprietary work of Kynetx Inc.
-# Your access to it does not grant you any rights, including, but not
-# limited to, the right to install, execute, copy, transcribe, reverse
-# engineer, or transmit it by any means.  Use of this Software is
-# governed by the terms of a Software License Agreement transmitted
-# separately.
+# KRE is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of
+# the License, or (at your option) any later version.
 #
-# Any reproduction, redistribution, or reverse engineering of the
-# Software not in accordance with the License Agreement is expressly
-# prohibited by law, and may result in severe civil and criminal
-# penalties. Violators will be prosecuted to the maximum extent
-# possible.
+# This program is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the GNU General Public License for more details.
 #
-# Without limiting the foregoing, copying or reproduction of the
-# Software to any other server or location for further reproduction or
-# redistribution is expressly prohibited, unless such reproduction or
-# redistribution is expressly permitted by the License Agreement
-# accompanying this Software.
-#
-# The Software is warranted, if at all, only according to the terms of
-# the License Agreement. Except as warranted in the License Agreement,
-# Kynetx Inc. hereby disclaims all warranties and conditions
-# with regard to the software, including all warranties and conditions
-# of merchantability, whether express, implied or statutory, fitness
-# for a particular purpose, title and non-infringement.
+# You should have received a copy of the GNU General Public
+# License along with this program; if not, write to the Free
+# Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307 USA
 #
 use strict;
 use warnings;
