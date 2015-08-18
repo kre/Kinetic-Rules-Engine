ed957c760e46a0a2d7f583df902dc856e141d0bf
PJW <pjw@byu.edu>
2015-08-18 12:42:50 -0600

    add system credential generation tool

---
+++ bin/system_credentials.pl
@@ -0,0 +1,66 @@
+#!/usr/bin/perl -w 
+#
+# This file is part of the Kinetic Rules Engine (KRE)
+# Copyright (C) 2007-2011 Kynetx, Inc. 
+#
+# KRE is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of
+# the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public
+# License along with this program; if not, write to the Free
+# Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307 USA
+#
+use lib qw(/web/lib/perl);
+use strict;
+use warnings;
+use Test::More;
+use Test::LongString;
+use Test::Deep;
+
+use Apache::Session::Memcached;
+use DateTime;
+use APR::URI;
+use APR::Pool ();
+use Cache::Memcached;
+use Storable 'dclone';
+
+# most Kyentx modules require this
+use Log::Log4perl qw(get_logger :levels);
+Log::Log4perl->easy_init($INFO);
+Log::Log4perl->easy_init($DEBUG);
+
+use Kynetx::Test qw/:all/;
+use Kynetx::Actions qw/:all/;
+use Kynetx::Modules::PCI qw/:all/;
+use Kynetx::Environments qw/:all/;
+use Kynetx::Session qw/:all/;
+use Kynetx::Configure qw/:all/;
+use Kynetx::Expressions qw/:all/;
+use Kynetx::Response qw/:all/;
+
+
+use Kynetx::FakeReq qw/:all/;
+use Kynetx::Util qw/:all/;
+
+
+use Data::Dumper;
+$Data::Dumper::Indent = 1;
+
+my $logger = get_logger();
+my $r = Kynetx::Test::configure();
+
+my $salt =  $ARGV[0];
+
+
+my $system_key = Kynetx::Modules::PCI::create_system_key($salt);
+
+print "System key: $system_key";
+
